87ab51e34eeae96872029786ac9bd974475ca709
LemonPrism <charliechenvan@gmail.com>
2025-11-20 13:25:39 -0800

    Add files via upload

---
+++ VigenereCipher.java
@@ -0,0 +1,57 @@
+
+public class VigenereCipher
+{
+    public static void main (String [] args){
+        System.out.println(encode ("ATTACKATDAWN", "LEMON") ); 
+        System.out.println (decode("LXFOPVEFRNHR" , "LEMON")); 
+    }
+  
+    public static String encode (String plainText, String keyword){
+        String str = ""; 
+        
+        for ( int i = 0 ; i < plainText.length(); i ++ ){
+            
+            
+            char letter = plainText.charAt(i);
+             
+            char key  = keyword.charAt(i%keyword.length()); 
+            
+            int nkey = key-65;
+            char newLetter = (char)((letter - 65 + nkey) % 26 + 65); 
+            
+            str+= newLetter ;
+             
+             
+             
+        }
+            
+        
+        return str; 
+    }
+    
+    public static String decode (String cipheredText, String keyword){
+        String str = ""; 
+        
+        for ( int i = 0 ; i < cipheredText.length(); i ++ ){
+            
+            
+            char letter = cipheredText.charAt(i);
+             
+            char key  = keyword.charAt(i%keyword.length()); 
+            
+            int nkey = key- 65;
+            char newLetter = (char)((letter + 65 - nkey) % 26 + 65); 
+            
+            str+= newLetter ;
+             
+             
+             
+        }
+            
+        
+        return str; 
+    }
+
+   
+}
+    
