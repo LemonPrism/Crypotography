c5edb2fd1c78a18980e6f526a4602e1e3ce4482a
LemonPrism <charliechenvan@gmail.com>
2025-11-20 13:59:27 -0800

    Add files via upload

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -5,53 +5,54 @@ public class VigenereCipher
         System.out.println(encode ("ATTACKATDAWN", "LEMON") ); 
         System.out.println (decode("LXFOPVEFRNHR" , "LEMON")); 
     }
-  
+
     public static String encode (String plainText, String keyword){
         String str = ""; 
-        
+
         for ( int i = 0 ; i < plainText.length(); i ++ ){
-            
-            
-            char letter = plainText.charAt(i);
-             
-            char key  = keyword.charAt(i%keyword.length()); 
-            
-            int nkey = key-65;
-            char newLetter = (char)((letter - 65 + nkey) % 26 + 65); 
-            
-            str+= newLetter ;
-             
-             
-             
+            if (Character.isLetter(plainText.charAt(i))){
+                if ( Character.isUpperCase( plainText.charAt(i))){
+                    char letter = plainText.charAt(i);
+
+                    char key  = keyword.charAt(i%keyword.length()); 
+
+                    int nkey = key-65;
+                    char newLetter = (char)((letter + 65 - nkey) % 26 + 65); 
+
+                    str+= newLetter ;
+                }else if ( Character.isLowerCase( plainText.charAt(i))){
+                    char letter = plainText.charAt(i);
+
+                    char key  = keyword.charAt(i%keyword.length()); 
+
+                    int nkey = key-97;
+                    char newLetter = (char)((letter + 97 - nkey) % 26 + 97); 
+
+                    str+= newLetter ; 
+                    
+                }
+
+            }else {
+                str += plainText.charAt(i);
+            }
         }
-            
-        
-        return str; 
-    }
-    
-    public static String decode (String cipheredText, String keyword){
-        String str = ""; 
-        
-        for ( int i = 0 ; i < cipheredText.length(); i ++ ){
-            
-            
-            char letter = cipheredText.charAt(i);
-             
-            char key  = keyword.charAt(i%keyword.length()); 
-            
-            int nkey = key- 65;
-            char newLetter = (char)((letter + 65 - nkey) % 26 + 65); 
-            
-            str+= newLetter ;
-             
-             
-             
-        }
-            
-        
         return str; 
     }
 
-   
+    public static String decode (String cipheredText, String keyword){
+        String str = ""; 
+
+        for ( int i = 0 ; i < cipheredText.length(); i ++ ){
+
+            char letter = cipheredText.charAt(i);
+            char key  = keyword.charAt(i%keyword.length()); 
+            int nkey = key- 65;
+            char newLetter = (char)((letter + 65 - nkey) % 26 + 65); 
+
+            str+= newLetter ;
+
+        }
+        return str; 
+    }
+
 }
-    
