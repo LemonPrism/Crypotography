fd512328ae53190035dc84c8737674e16c12e683
LemonPrism <charliechenvan@gmail.com>
2025-11-22 23:08:52 -0800

    Add files via upload

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -1,58 +1,61 @@
-
 public class VigenereCipher
 {
     public static void main (String [] args){
-        System.out.println(encode ("ATTACKATDAWN", "LEMON") ); 
-        System.out.println (decode("LXFOPVEFRNHR" , "LEMON")); 
+        System.out.println(encode("ATTACK at DAWN", "LEMON")); 
+        System.out.println(decode("LXFOPVEFRNHR", "LEMON")); 
     }
 
     public static String encode (String plainText, String keyword){
         String str = ""; 
 
-        for ( int i = 0 ; i < plainText.length(); i ++ ){
+        for (int i = 0 ; i < plainText.length(); i++){
             if (Character.isLetter(plainText.charAt(i))){
-                if ( Character.isUpperCase( plainText.charAt(i))){
-                    char letter = plainText.charAt(i);
+                char letter = plainText.charAt(i);
+                char key  = keyword.charAt(i % keyword.length());
 
-                    char key  = keyword.charAt(i%keyword.length()); 
-
-                    int nkey = key-65;
-                    char newLetter = (char)((letter + 65 - nkey) % 26 + 65); 
-
-                    str+= newLetter ;
-                }else if ( Character.isLowerCase( plainText.charAt(i))){
-                    char letter = plainText.charAt(i);
-
-                    char key  = keyword.charAt(i%keyword.length()); 
-
-                    int nkey = key-97;
-                    char newLetter = (char)((letter + 97 - nkey) % 26 + 97); 
-
-                    str+= newLetter ; 
-                    
+                if (Character.isUpperCase(letter)){
+                    int nkey = Character.toUpperCase(key) - 65;
+                    char newLetter = (char)((letter - 65 + nkey + 26) % 26 + 65);
+                    str += newLetter;
+                }
+                else {
+                    int nkey = Character.toLowerCase(key) - 97;
+                    char newLetter = (char)((letter - 97 + nkey + 26) % 26 + 97);
+                    str += newLetter;
                 }
 
-            }else {
+            } else {
                 str += plainText.charAt(i);
             }
         }
+
         return str; 
     }
 
     public static String decode (String cipheredText, String keyword){
         String str = ""; 
 
-        for ( int i = 0 ; i < cipheredText.length(); i ++ ){
+        for (int i = 0 ; i < cipheredText.length(); i++){
+            if (Character.isLetter(cipheredText.charAt(i))){
+                char letter = cipheredText.charAt(i);
+                char key  = keyword.charAt(i % keyword.length());
 
-            char letter = cipheredText.charAt(i);
-            char key  = keyword.charAt(i%keyword.length()); 
-            int nkey = key- 65;
-            char newLetter = (char)((letter + 65 - nkey) % 26 + 65); 
+                if (Character.isUpperCase(letter)){
+                    int nkey = Character.toUpperCase(key) - 65;
+                    char newLetter = (char)(((letter - 65 - nkey + 26) % 26) + 65);
+                    str += newLetter;
+                }
+                else {
+                    int nkey = Character.toLowerCase(key) - 97;
+                    char newLetter = (char)(((letter - 97 - nkey + 26) % 26) + 97);
+                    str += newLetter;
+                }
 
-            str+= newLetter ;
-
+            } else {
+                str += cipheredText.charAt(i);
+            }
         }
+
         return str; 
     }
-
 }
